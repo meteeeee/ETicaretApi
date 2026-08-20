1cb25f1412dbcb1af5da3f19e394310c8035cbda
meteeeee <mete.sezer2004@gmail.com>
2026-08-20 11:45:37 +0300

    Configured swagger.

--- Presentation/ETicaretApi.WebApi/Program.cs
+++ Presentation/ETicaretApi.WebApi/Program.cs
@@ -1,17 +1,46 @@
+using ETicaretApi.Application.Features.CQRSDesignPattern.Handlers.CategoryHandlers;
+using ETicaretApi.Application.Features.CQRSDesignPattern.Handlers.ProductHandlers;
+using ETicaretApi.Persistence.Context;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// DbContext
+builder.Services.AddDbContext<ProductContext>();
+
+// Category Handlers
+builder.Services.AddScoped<getCategoryQueryHandler>();
+builder.Services.AddScoped<getCategoryByIdQueryHandler>();
+builder.Services.AddScoped<CreateCategoryCommandHandler>();
+builder.Services.AddScoped<UpdateCategoryCommandHandler>();
+builder.Services.AddScoped<RemoveCategoryCommandHandler>();
+
+// Product Handlers
+builder.Services.AddScoped<getProductQueryHandler>();
+builder.Services.AddScoped<getProductByIdQueryHandler>();
+builder.Services.AddScoped<CreateProductCommandHandler>();
+builder.Services.AddScoped<UpdateProductCommandHandler>();
+builder.Services.AddScoped<RemoveProductCommandHandler>();
+
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My Api", Version = "v1" });
+});
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My Api V1");
+    });
 }
 
 app.UseHttpsRedirection();
