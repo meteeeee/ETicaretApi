cffc2c266080bf4ad86f05d6591f00e4d04b99e3
meteeeee <mete.sezer2004@gmail.com>
2026-08-20 13:43:00 +0300

    .

--- Infrastructure/ETicaretApi.Persistence/Migrations/20260820090715_mig2_fix_category_type.Designer.cs
+++ Infrastructure/ETicaretApi.Persistence/Migrations/20260820090715_mig2_fix_category_type.Designer.cs
@@ -3,6 +3,7 @@ using ETicaretApi.Persistence.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ETicaretApi.Persistence.Migrations
 {
     [DbContext(typeof(ProductContext))]
-    partial class ProductContextModelSnapshot : ModelSnapshot
+    [Migration("20260820090715_mig2_fix_category_type")]
+    partial class mig2_fix_category_type
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
